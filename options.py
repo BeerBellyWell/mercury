c13b37c96e27162d808db35968c0d49b1dc1ade5
Андрей Чернышев <candreey17@mail.ru>
2023-12-11 20:36:46 +1200

    auto update driver

--- options.py
+++ options.py
@@ -3,13 +3,17 @@ import time
 
 from dotenv import load_dotenv
 from selenium import webdriver
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
 
 
 load_dotenv()
 LOGIN = os.getenv('LOGIN')
 PASSWORD = os.getenv('PASS')
 URL = os.getenv('URL')
-driver = webdriver.Chrome()
+
+service = Service(executable_path=ChromeDriverManager().install())
+driver = webdriver.Chrome(service=service)
 
 
 def time_sleep_1():
